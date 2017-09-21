e5e05507fc6f0d9e28edd93c719a2ccbe8cc63e3
beginner-g <969925523@qq.com>
2017-09-21 16:19:59 +0800

    1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './app.css';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import Main from '../Main/Main'
 import Layout from '../Layout/Layout'
 class App extends Component {
